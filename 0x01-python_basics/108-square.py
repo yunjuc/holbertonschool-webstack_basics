0f7b680e27875d5eb3bff21acfd34462d6a64189
Yunju Chen <info@yunjuchen.net>
2019-02-02 22:42:13 +0000

    Fix style

--- 0x01-python_basics/108-square.py
+++ 0x01-python_basics/108-square.py
@@ -31,12 +31,12 @@ class Square:
     @position.setter
     def position(self, value):
         '''position setter'''
-        if type(value) != tuple or len(value)!= 2:
+        if type(value) != tuple or len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
         if type(value[0]) != int or type(value[1]) != int:
             raise TypeError("position must be a tuple or 2 positive integers")
         if value[0] < 0 or value[1] < 0:
-            raise TypeError("position must be a tuple or 2 positive integers") 
+            raise TypeError("position must be a tuple or 2 positive integers")
         self.__position = value
 
     def area(self):
