22a87cb0b380c0e9fc77cea769c924878c9d3951
Yunju Chen <info@yunjuchen.net>
2019-02-02 22:43:01 +0000

    Return None is dict is empty

--- 0x01-python_basics/105-best_score.py
+++ 0x01-python_basics/105-best_score.py
@@ -4,8 +4,8 @@
 def best_score(my_dict):
     '''return the key with the biggest value pair in the dict'''
 
-    if type(my_dict) is not dict:
-        return
+    if type(my_dict) is not dict or len(my_dict) == 0:
+        return None
     m = max(my_dict.values())
     for k, v in my_dict.items():
         if v == m:
