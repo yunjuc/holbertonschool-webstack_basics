862a52933cce0f238a474b274a63fdb581f3a73d
Yunju Chen <info@yunjuchen.net>
2019-02-03 08:59:32 +0000

    Check if idx is out of range

--- 0x01-python_basics/6-replace_in_list.py
+++ 0x01-python_basics/6-replace_in_list.py
@@ -4,7 +4,7 @@
 def replace_in_list(my_list, idx, element):
     '''replace list element by position'''
 
-    if idx > len(my_list)-1:
+    if idx < 0 or idx > len(my_list)-1:
         return my_list
     else:
         my_list[idx] = element
