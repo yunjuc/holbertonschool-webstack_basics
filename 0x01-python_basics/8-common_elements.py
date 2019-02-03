46bc51f217404c3e61457766fb84842012756d8c
Yunju Chen <info@yunjuchen.net>
2019-02-03 09:34:30 +0000

    Fix bug

--- 0x01-python_basics/8-common_elements.py
+++ 0x01-python_basics/8-common_elements.py
@@ -4,8 +4,8 @@
 def common_elements(set_1, set_2):
     '''find common elements in two sets'''
 
+    res = []
     for i in set_1:
-        for j in set_2:
-            if i == j:
-                return i
-    return None
+        if i in set_2:
+            res.append(i)
+    return res
