08e49506b67b2cecd027ad9b5cbb6f6a88eb4cf9
Yunju Chen <info@yunjuchen.net>
2019-02-09 04:39:50 +0000

    Fix typo

--- 0x02-javascript_basics/3-to_integer.js
+++ 0x02-javascript_basics/3-to_integer.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 if (!parseInt(process.argv[2])) {
-  console.log('No a number');
+  console.log('Not a number');
 } else {
   console.log(`My number: ${parseInt(process.argv[2])}`);
 }
