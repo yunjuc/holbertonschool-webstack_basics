8eaf14ffb61c1d8886b691cfc8b541ae517dcdcd
Yunju Chen <info@yunjuchen.net>
2019-02-09 07:23:34 +0000

    Use prototype and replace arrow function

--- 0x02-javascript_basics/110-square.js
+++ 0x02-javascript_basics/110-square.js
@@ -2,18 +2,18 @@
 const base = require('./109-square').Square;
 
 function Square (size) {
-  base.call(this, size, size);
-
-  this.charPrint = (x) => {
-    if (x) {
-      for (let i = 0; i < this.width; i++) {
-        for (let j = 0; j < this.width; j++) {
-          process.stdout.write('C');
-        }
-        console.log();
-      }
-    } else this.print();
-  };
+  base.call(this, size);
 }
+
+Square.prototype.charPrint = function (c) {
+  if (c) {
+    for (let i = 0; i < this.width; i++) {
+      for (let j = 0; j < this.width; j++) {
+        process.stdout.write(c);
+      }
+      console.log();
+    }
+  } else this.print();
+};
 
 module.exports = { Square };
