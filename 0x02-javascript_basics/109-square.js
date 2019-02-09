95edc9d4784c098ed93c7e621dff10296232a5fa
Yunju Chen <info@yunjuchen.net>
2019-02-09 05:48:26 +0000

    Refactor Rectangle class

--- 0x02-javascript_basics/109-square.js
+++ 0x02-javascript_basics/109-square.js
@@ -4,13 +4,10 @@ const Rectangle = require('./9-rectangle').Rectangle;
 function Square (size) {
   Rectangle.call(this, size, size);
 
-  Square.prototype.double = () => {
+  this.double = () => {
     this.width *= 2;
     this.height *= 2;
   };
 }
-
-Square.prototype = Object.create(Rectangle.prototype);
-Square.prototype.constructor = Square;
 
 module.exports = { Square };
