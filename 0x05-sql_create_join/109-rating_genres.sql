daa66e398cfc3e869ad235ad3432267219545761
Yunju Chen <info@yunjuchen.net>
2019-02-22 17:52:51 +0000

    Fix style

--- 0x05-sql_create_join/109-rating_genres.sql
+++ 0x05-sql_create_join/109-rating_genres.sql
@@ -1,5 +1,5 @@
 -- show all genres by rating
-SELECT tv_genres.name, SUM(tv_show_ratings.rate) as rating
+SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating
 FROM tv_genres
 LEFT JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
 LEFT JOIN tv_show_ratings ON tv_show_genres.show_id = tv_show_ratings.show_id
