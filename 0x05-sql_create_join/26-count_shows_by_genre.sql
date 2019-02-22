0bea876b643ef3b2c18486ef64b5ed49dc82d81c
Yunju Chen <info@yunjuchen.net>
2019-02-22 18:37:00 +0000

    Group by genre_id

--- 0x05-sql_create_join/26-count_shows_by_genre.sql
+++ 0x05-sql_create_join/26-count_shows_by_genre.sql
@@ -2,5 +2,5 @@
 SELECT tv_genres.name, COUNT(tv_show_genres.genre_id) AS number_shows
 FROM tv_genres
 LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-GROUP BY tv_genres.name
+GROUP BY tv_genres.id
 ORDER BY number_shows DESC;
